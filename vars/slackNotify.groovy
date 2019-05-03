b607e8d3e287878a0f3e8e19af1bf11c5b7486ba
Gradecak <gradecam@tcd.ie>
2019-05-03 13:11:06 +0200

    Change messages posted to slack

--- vars/slackNotify.groovy
+++ vars/slackNotify.groovy
@@ -1,16 +1,19 @@
 #!/usr/bin/env groovy
 
 def call(String buildResult) {
-  if ( buildResult == "SUCCESS" ) {
-    slackSend(color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful")
-  }
-  else if( buildResult == "FAILURE" ) {
-    slackSend(color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed")
-  }
-  else if( buildResult == "UNSTABLE" ) {
-    slackSend(color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable")
-  }
-  else {
-    slackSend(color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear")
-  }
+    if ( buildResult == "SUCCESS" ) {
+        slackSend(color: "good", message: "Job: ${env.JOB_NAME}-${env.BUILD_NUMBER} was successful!")
+    }
+    else if( buildResult == "FAILURE" ) {
+        slackSend(color: "danger", message: "Job: ${env.JOB_NAME}-${env.BUILD_NUMBER} failed!")
+    }
+    else if( buildResult == "UNSTABLE" ) {
+        slackSend(color: "warning", message: "Job: ${env.JOB_NAME}-${env.BUILD_NUMBER} is unstable.")
+    }
+    else if (buildResult == "STARTED") {
+        slackSend(color: "ff9900", message: "Job: ${env.JOB_NAME}-${env.BUILD_NUMBER} started...")
+    }
+    else {
+        slackSend(color: "danger", message: "Job: ${env.JOB_NAME}-${env.BUILD_NUMBER} result unclear")
+    }
 }
